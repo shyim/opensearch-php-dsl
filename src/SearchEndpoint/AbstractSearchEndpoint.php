67554d8ce5e07d4ea012df70b74a58172ad74c99
Julian Vennen <julian@vennen.me>
2026-04-08 13:11:07 +0200

    Change order of operations instead of checking empty string

--- src/SearchEndpoint/AbstractSearchEndpoint.php
+++ src/SearchEndpoint/AbstractSearchEndpoint.php
@@ -33,12 +33,12 @@ abstract class AbstractSearchEndpoint extends AbstractNormalizable
 
     public function add(BuilderInterface $builder, ?string $key = null): string
     {
-        if (array_key_exists($key ?? '', $this->container)) {
-            throw new \OverflowException(sprintf('Builder with %s name for endpoint has already been added!', $key));
-        }
-
         if (!$key) {
             $key = bin2hex(random_bytes(self::KEY_LENGTH));
+        }
+
+        if (array_key_exists($key, $this->container)) {
+            throw new \OverflowException(sprintf('Builder with %s name for endpoint has already been added!', $key));
         }
 
         $this->container[$key] = $builder;
